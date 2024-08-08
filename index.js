44430baf08e1b44cc0abddb9493af678042d7da3
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 10:24:29 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// Get started!
+
+const name = prompt("What's your name?")
+console.log("Welcome to my comedy game, " + name)
+
+let meme = prompt("What meme feel like picking right now? (Meme 1, Meme 2, Meme 3, Meme 4, or Meme 5)")
+
+console.log("You picked meme " + meme)
+
